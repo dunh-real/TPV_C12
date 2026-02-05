d38389fec32aef5d19d12607d447c69cb62f2913
Ximm0502 <daphong2k4@gmail.com>
2026-02-05 16:42:37 +0700

    update src/services/ocr_service.py

--- src/services/ocr_service.py
+++ src/services/ocr_service.py
@@ -59,6 +59,7 @@ from vllm import LLM, SamplingParams
 from PIL import Image
 import time
 import os
+import fitz
 
 IMAGE_PATH = "../../data/images/page_1.png"
 MODEL_ID = "lightonai/LightOnOCR-2-1B"
@@ -107,6 +108,11 @@ inputs = {
     "multi_modal_data": {"image": image},
 }
 
+# 4. Chay warmup
+print('-'*70)
+llm.generate([inputs], sampling_params = SamplingParams(max_tokens = 10))
+print('-'*70)
+
 
 # 5. Chay that
 start_time = time.perf_counter()
@@ -119,3 +125,36 @@ for o in outputs:
     print(o.outputs[0].text.strip())
     
 print(f"Executed time: {ex_time:.4f} seconds")
+
+
+
+class OCRService:
+    def __init__(self):
+        self.image_dir = "../../data/images"
+        self.model_name = "lightonai/LightOnOCR-2-1B"
+        self.model = LLM(
+            model = self.model_name,
+            dtype = "bfloat16",
+            trust_remote_code = True,
+            limit_mm_per_prompt = {"image": 1},
+            gpu_memory_utilization = 0.9,
+            enforce_eager = True
+        )
+    
+    def convert_pdf_to_img(self, image_path):
+        doc = fitz.open(image_path)
+        for page_num in range(doc.page_count):
+            page = doc.load_page(page_num)
+            pix = page.get_pixmap(dpi=300)
+            image_path = os.path.join(IMAGE_PATH, f"page_{page_num+1}.png")
+            pix.save(image_path)
+            print(f"Saved {image_path}")
+        
+            doc.close()
+        return None
+    
+    def image_preprocess(self, image):
+        return None
+    
+    def run_ocr(self, image):
+        return None
